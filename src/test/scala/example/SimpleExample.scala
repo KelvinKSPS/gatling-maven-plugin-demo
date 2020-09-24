5fbd72a6ee193834c00aa18bef09f00327c4bc98
Kelvin Silva <kr.silva@samsung.com>
2020-09-24 02:44:35 -0300

    Uploading a Simple Example

---
+++ src/test/scala/example/SimpleExample.scala
@@ -0,0 +1,22 @@
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration._
+
+class SimpleTest extends Simulation {
+    
+    val httpConf = http.baseUrl("http://www.google.com.br/")
+        .header("Accept", "application/json")
+
+    val demoScenario = scenario("Sanity")
+        .exec(http("Getting all games")
+        .get("search?q=videogames")
+         )
+
+    setUp(
+        demoScenario.inject(
+               rampConcurrentUsers(0) to 20 during (20 seconds)
+        )
+    ).protocols(httpConf)
+
+}
